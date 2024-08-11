3736b20ad44cd0a5c3817c6db2a0cb79645cd539
kirinnee <kirinnee97@gmail.com>
2024-08-11 15:25:57 +0800

    upstream: upgrade nix, helm upstream, GHA

--- nix/packages.nix
+++ nix/packages.nix
@@ -1,4 +1,4 @@
-{ pkgs, atomi, pkgs-sep-24-23 }:
+{ pkgs, atomi, pkgs-240810 }:
 let
 
   all = {
@@ -6,13 +6,12 @@ let
       with atomi;
       {
         inherit
-          infisical
           pls
           sg;
       }
     );
-    sep-24-23 = (
-      with pkgs-sep-24-23;
+    pkgs240810 = (
+      with pkgs-240810;
       {
         inherit
           coreutils
@@ -25,6 +24,7 @@ let
 
           # lint
           treefmt
+          infisical
 
           # infra
           k3d
@@ -41,4 +41,4 @@ let
 in
 with all;
 atomipkgs //
-sep-24-23
+pkgs240810
